7129f3362006b2007c8747b1d92af011349f544a
pawelciupka <pawelciupka@gmail.com>
2019-07-31 19:32:36 +0200

    style update vol1

--- react-ui/src/components/main.js
+++ react-ui/src/components/main.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { InputGroup, FormControl, Button } from "react-bootstrap";
+import { InputGroup, FormControl, Button, Spinner } from "react-bootstrap";
 import ScannerContainer from "./scannerContainer";
 
 class Main extends React.Component {
@@ -8,28 +8,40 @@ class Main extends React.Component {
 
     this.state = {
       isAuth: false,
-      password: ""
+      password: "",
+      authMessage: ""
     };
+    this.password = React.createRef();
 
     this.authorize = this.authorize.bind(this);
+  }
+
+  handlePasswordChange() {
+    this.setState({ password: this.password.current.value });
   }
 
   render() {
     return (
       <>
         {!this.state.isAuth ? (
-          <>
+          <div className="auth-container">
             <InputGroup className="mb-3">
               <FormControl
                 placeholder="Hasło"
                 aria-label="Hasło"
                 aria-describedby="basic-addon3"
+                type="password"
+                ref={this.password}
+                onChange={() => this.handlePasswordChange()}
               />
             </InputGroup>
             <Button variant="primary" onClick={this.authorize}>
               Autoryzuj
             </Button>
-          </>
+            <div className="authMessage">
+              <p>{this.state.authMessage}</p>
+            </div>
+          </div>
         ) : null}
 
         {this.state.isAuth ? <ScannerContainer /> : null}
@@ -38,7 +50,12 @@ class Main extends React.Component {
   }
 
   authorize() {
-    this.setState({ isAuth: !this.state.isAuth });
+    this.setState({ authMessage: "" });
+    if (this.state.password === "xd") {
+      this.setState({ isAuth: !this.state.isAuth });
+    } else {
+      this.setState({ authMessage: "Nieprawidłowe hasło" });
+    }
   }
 }
 
